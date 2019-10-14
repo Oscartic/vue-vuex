330daee774209728568271a46a4bd7e77ce9f854
OscarB <oscartic@gmail.com>
2019-10-13 21:23:41 -0300

    MovieItem and MoviList components was created and was included into MovieApp
    :rocket:

---
+++ src/components/MovieList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="row" v-if="movies.length > 0"> 
+            <movie :movie="movie" v-for="movie in movies" :key="movie.id"></movie>
+        </div>
+        <div v-else="" class="alert alert-danger">No hay películas disponibles</div>
+    </div>
+</template>
+<script>
+    import Movie from './MovieItem';
+    import { mapGetters } from 'vuex';
+
+    export default {
+        components: {
+            Movie
+        },
+        computed: {
+            ...mapGetters({
+                movies: 'filteredMovies',
+            })
+        }
+    }
+</script>
--- src/components/MoviesApp.vue
+++ src/components/MoviesApp.vue
@@ -2,13 +2,17 @@
     <div>
         <h1 class="text-center text-muted">Buscador con Vuex</h1>
         <search-form></search-form>
+        <hr>
+        <Movie-list></Movie-list>
     </div>
 </template>
 <script>
     import SearchForm from './SearchForm';
+    import MovieList from './MovieList';
     export default {
         components: {
-            SearchForm
+            SearchForm,
+            MovieList
         }
     }
 </script>
